49180adc9bb8350b470782972519f5992bd7d92e
fog <brian.fogarty98@gmail.com>
2017-11-12 15:52:57 +1100

    Initial project commit

---
+++ Swin-Adventure/Swin-Adventure/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swin_Adventure
+{
+    public class Item : GameObject
+    {
+        public Item(string[] idents, string name, string desc) : base(idents, name, desc) { }
+
+    }
+}
---
+++ Swin-Adventure/Swin-Adventure/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Swin_Adventure
+{
+    public class Player : GameObject
+    {
+        private Inventory _inventory;
+
+        public Player(string name, string desc) : base(new string[] { "me", "inventory"}, name, desc)
+        {
+            _inventory = new Inventory();
+        }
+
+        public GameObject Locate(string id)
+        {
+            if(AreYou(id))
+            {
+                return this;
+            }
+            else
+            {
+                return _inventory.Fetch(id);
+            }
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                string desc = "You are carrying\n";
+                desc +=  _inventory.ItemList;
+                return desc;
+            }
+        }
+
+        public Inventory Inventory { get => _inventory; }
+    }
+}
